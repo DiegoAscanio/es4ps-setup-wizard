1f62197b7a19c5352ba2d15505ea9bd71f26262b
diegoascanio <6247962+DiegoAscanio@users.noreply.github.com>
2024-04-28 18:14:00 -0300

    adicionando validators

---
+++ es4ps-setup-wizard/src/components/validators.js
@@ -0,0 +1,32 @@
+const isEmpty = (value) =>
+    value === undefined || value === null || value === "";
+
+const isEmailAddressValid = (email) =>
+    email.match(/^([\w.%+-]+)@([\w-]+\.)+([\w]{2,})$/i) !== null;
+
+const isIPAddressValid = (ip) =>
+    ip.match(/^([0-9]{1,3}\.){3}[0-9]{1,3}$/) !== null;
+
+const isPasswordValid = (password1, password2) => {
+    // password should be at least 8 characters long
+    // password should contain at least one number
+    // password should contain at least one special character
+    // password should contain at least one uppercase letter
+    // password should contain at least one lowercase letter
+    // password should match the confirmation password
+    let condition = true;
+    condition &= password1.length >= 8;
+    condition &= password1.match(/[0-9]/) !== null;
+    condition &= password1.match(/[!@#$%^&*]/) !== null;
+    condition &= password1.match(/[A-Z]/) !== null;
+    condition &= password1.match(/[a-z]/) !== null;
+    condition &= password1 === password2;
+    return Boolean(condition);
+};
+
+export {
+    isEmpty,
+    isEmailAddressValid,
+    isIPAddressValid,
+    isPasswordValid
+};
